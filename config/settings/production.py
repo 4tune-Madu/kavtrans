debba15d9dadeaada2689d87c55b8a3553370c19
Fortune <fortunemadu23@gmail.com>
2026-03-30 16:10:49 +0100

    updated production.py database

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,4 @@
+# POSTGRES DATABASE (Render ENV VARIABLES)
 from .base import *
 import os
 
@@ -7,15 +8,14 @@ ALLOWED_HOSTS = ['.onrender.com']
 
 
 # POSTGRES DATABASE (Render ENV VARIABLES)
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -38,6 +38,25 @@ cloudinary.config(
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-#CLOUDINARY_CLOUD_NAME="dvuv7kz5p"
-#CLOUDINARY_API_KEY="849246665983819"
-#CLOUDINARY_API_SECRET="f97l2q384M_FCzHSpN2OnK7aVzY"
+
+
+
+# SECURITY SETTINGS
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+# Recommended for Render
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import cloudinary
+
+cloudinary.config(
+    cloud_name = os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key = os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET')
+)
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
